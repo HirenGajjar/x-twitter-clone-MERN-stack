1c7e48206ef2f0dd48df285cc76cb50038fdebd6
HirenGajjar <gajjarhiren111@gmail.com>
2024-06-21 16:20:16 -0700

    Add password hashing in controller

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -1,6 +1,59 @@
+import UserModel from "../DB/Models/user.model.js";
+import bcrypt from "bcryptjs";
+// Signup controller
 const signinController = async (req, res) => {
   try {
-  } catch (error) {}
+    // Get all the values
+    const { fullname, username, email, password } = req.body;
+    // Regex for email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //Email validation
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email!" });
+    }
+    //Check for existing user
+    const existingUser = await UserModel.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username not available!" });
+    }
+    // Check for existing email
+    const existingEmail = await UserModel.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email already exist!" });
+    }
+    // Hash password
+    // Salt
+    const salt = await bcrypt.genSalt(10);
+    // Hash password
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const newUser = new UserModel({
+      fullname,
+      username,
+      password: hashPassword,
+      email,
+    });
+    // once new user is signed up generate jsonwebtoken
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      // Save new user to DB
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImage: newUser.profileImage,
+        coverImage: newUser.coverImage,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error!" });
+  }
 };
 const signupController = async (req, res) => {
   try {
--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,8 @@ import connectDB from "./DB/connect.db.js";
 dotenv.config();
 import authRoutes from "./Routes/auth.route.js";
 const app = express();
-
+//Middlewares
+app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 // DB Connection
